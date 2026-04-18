13a4118877f9d0515a8327d3b2e928069a05cb06
xorl-ldaf <i.s.safin1406@gmail.com>
2026-04-18 23:27:02 +0300

    fix gradle config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.diagnostics.DependencyInsightReportTask
+
 plugins {
     base
 }
@@ -12,3 +14,10 @@ subprojects {
 
 group = "com.example.todo"
 version = "0.1.0-SNAPSHOT"
+
+extra["tomcat.version"] = "11.0.21"
+extra["jackson-bom.version"] = "3.1.1"
+
+tasks.named<DependencyInsightReportTask>("dependencyInsight") {
+    configuration = project(":apps:web-app").configurations.named("runtimeClasspath").get()
+}
